d74242818415dd1016643bc131fe5326bbc6925d
Silas Bechet <silasbechet@gmail.com>
2021-03-04 15:08:13 +0100

    ad entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+services=('php-fpm','httpd','mariadb','centreon','cbd','centengine','gorgoned','snmptrapd','centreontrapd','snmpd')
+function main()
+    {
+        for i in "${services[@]}"
+        do
+            test=1
+            i
+            if ($? -eq 0)
+            then
+                test=0
+            fi
+            while (test -eq 1); do
+              sleep 1
+            done
+        done
+    }
+main()
+centreon
+
+
+
