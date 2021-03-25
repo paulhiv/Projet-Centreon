ce28fde13723d7da5671f85188f8eeaf6cc154a1
paulhiv <paul.hivert@gmail.com>
2021-03-25 11:28:33 +0100

    fix

--- centreon-central/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-systemctl enable rh-php72-php-fpm httpd24-httpd mariadb centreon cbd centengine gorgoned snmptrapd centreontrapd snmpd
-systemctl restart rh-php72-php-fpm.service
-systemctl start httpd24-httpd
