9a21c567fb03a577cab4483e5deb17f840db8b7a
paulhiv <paul.hivert@gmail.com>
2021-03-25 13:23:00 +0100

    fixes

--- centreon-all/entrypoint.sh
+++ centreon-all/entrypoint.sh
@@ -4,3 +4,7 @@ systemctl enable rh-php72-php-fpm httpd24-httpd mariadb centreon cbd centengine 
 systemctl restart rh-php72-php-fpm.service
 systemctl restart mysql
 systemctl start httpd24-httpd
+while true
+do
+	sleep 1
+done
