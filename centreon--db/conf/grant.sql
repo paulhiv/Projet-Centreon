4dcaf3baac5c16d735e7898cb9206c03ef65cf97
paulhiv <paul.hivert@gmail.com>
2021-03-25 11:14:25 +0100

    merge prep

--- centreon--db/conf/grant.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'admin'@'centreon-central' IDENTIFIED BY 'Azerty';
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'centreon-central' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
--- centreon-all/entrypoint.sh
+++ centreon-all/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+systemctl daemon-reload
 systemctl enable rh-php72-php-fpm httpd24-httpd mariadb centreon cbd centengine gorgoned snmptrapd centreontrapd snmpd
 systemctl restart rh-php72-php-fpm.service
+systemctl restart mysql
 systemctl start httpd24-httpd
