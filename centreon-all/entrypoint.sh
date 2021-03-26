003773aeec643e238baa32c7e6fba76f09d6bfc2
Silas <root@localhost.localdomain>
2021-03-26 12:06:24 +0100

    11

--- centreon-all/entrypoint.sh
+++ centreon-all/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-systemctl daemon-reload
-systemctl enable rh-php72-php-fpm httpd24-httpd mariadb centreon cbd centengine gorgoned snmptrapd centreontrapd snmpd
-systemctl restart rh-php72-php-fpm.service
-systemctl restart mysql
-systemctl start httpd24-httpd
-while true
-do
-	sleep 1
-done
+if [$1 -eq 'start']; then
+	/usr/sbin/init 
+	systemctl daemon-reload
+	systemctl enable rh-php72-php-fpm httpd24-httpd mariadb centreon cbd centengine gorgoned snmptrapd centreontrapd snmpd
+	systemctl restart rh-php72-php-fpm.service
+	systemctl restart mysql
+	systemctl start httpd24-httpd
+	while true
+	do
+		journalctl
+	done
+fi
